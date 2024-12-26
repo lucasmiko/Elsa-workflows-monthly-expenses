983c35ee268789cfcbaaadba01854640a6c75253
Lucas Mikó Corrêa <LCORREA@BETHEL.JW.ORG>
2024-12-26 11:51:39 -0500

    Add project files.

---
+++ Elsa-workflows-monthly-expenses/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using Elsa.Activities.Console;
+using Elsa.Builders;
+
+namespace Elsa_workflows_monthly_expenses
+{
+    /// <summary>
+    /// A basic workflow with just one WriteLine activity.
+    /// </summary>
+    public class HelloWorld : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder) => builder.WriteLine("Hello World!");
+    }
+}
---
+++ Elsa-workflows-monthly-expenses/Program.cs
@@ -0,0 +1,24 @@
+﻿using Elsa.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa_workflows_monthly_expenses
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+            // Create a service container with Elsa services.
+            var services = new ServiceCollection()
+                .AddElsa(options => options
+                    .AddConsoleActivities()
+                    .AddWorkflow<HelloWorld>())
+                .BuildServiceProvider();
+
+            // Get a workflow runner.
+            var workflowRunner = services.GetRequiredService<IBuildsAndStartsWorkflow>();
+
+            // Run the workflow.
+            await workflowRunner.BuildAndStartWorkflowAsync<HelloWorld>();
+        }
+    }
+}
